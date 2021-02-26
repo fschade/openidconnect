ef69ca2e1d3d1f5400940237e36d65622faeac33
Florian Schade <f.schade@icloud.com>
2021-02-26 16:29:25 +0100

    add ms-teams flow

--- lib/Client.php
+++ lib/Client.php
@@ -28,139 +28,124 @@ use OCP\IURLGenerator;
 
 class Client extends OpenIDConnectClient {
 
-	/** @var ISession */
-	private $session;
-	/** @var IConfig */
-	private $config;
-	/** @var array */
-	private $wellKnownConfig;
-	/**
-	 * @var IURLGenerator
-	 */
-	private $generator;
+    /** @var ISession */
+    private $session;
+    /** @var IConfig */
+    private $config;
+    /** @var array */
+    private $wellKnownConfig;
+    /**
+     * @var IURLGenerator
+     */
+    private $generator;
 
-	/**
-	 * Client constructor.
-	 *
-	 * @param IConfig $config
-	 * @param IURLGenerator $generator
-	 * @param ISession $session
-	 */
-	public function __construct(IConfig $config,
-								IURLGenerator $generator,
-								ISession $session
-	) {
-		$this->session = $session;
-		$this->config = $config;
-		$this->generator = $generator;
+    /**
+     * Client constructor.
+     *
+     * @param IConfig $config
+     * @param IURLGenerator $generator
+     * @param ISession $session
+     */
+    public function __construct(IConfig $config,
+                                IURLGenerator $generator,
+                                ISession $session
+    ) {
+        $this->session = $session;
+        $this->config = $config;
+        $this->generator = $generator;
 
-		$openIdConfig = $this->getOpenIdConfig();
-		if ($openIdConfig === null) {
-			return;
-		}
-		parent::__construct(
-			$openIdConfig['provider-url'],
-			$openIdConfig['client-id'],
-			$openIdConfig['client-secret']
-		);
-		$scopes = $openIdConfig['scopes'] ?? ['openid', 'profile', 'email'];
-		$this->addScope($scopes);
+        $openIdConfig = $this->getOpenIdConfig();
+        if ($openIdConfig === null) {
+            return;
+        }
+        parent::__construct(
+            $openIdConfig['provider-url'],
+            $openIdConfig['client-id'],
+            $openIdConfig['client-secret']
+        );
+        $scopes = $openIdConfig['scopes'] ?? ['openid', 'profile', 'email'];
+        $this->addScope($scopes);
 
-		$insecure = $openIdConfig['insecure'] ?? false;
-		if ($insecure) {
-			$this->setVerifyHost(false);
-			$this->setVerifyPeer(false);
-		}
-		// set config parameters in case well known is not supported
-		if (isset($openIdConfig['provider-params'])) {
-			$this->providerConfigParam($openIdConfig['provider-params']);
-		}
-		// set additional auth parameters
-		if (isset($openIdConfig['auth-params'])) {
-			$this->addAuthParam($openIdConfig['auth-params']);
-		}
-	}
+        $insecure = $openIdConfig['insecure'] ?? false;
+        if ($insecure) {
+            $this->setVerifyHost(false);
+            $this->setVerifyPeer(false);
+        }
+        // set config parameters in case well known is not supported
+        if (isset($openIdConfig['provider-params'])) {
+            $this->providerConfigParam($openIdConfig['provider-params']);
+        }
+        // set additional auth parameters
+        if (isset($openIdConfig['auth-params'])) {
+            $this->addAuthParam($openIdConfig['auth-params']);
+        }
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getOpenIdConfig() {
-		return $this->config->getSystemValue('openid-connect', null);
-	}
+    /**
+     * @return mixed
+     */
+    public function getOpenIdConfig() {
+        return $this->config->getSystemValue('openid-connect', null);
+    }
 
-	/**
-	 * @throws \Jumbojett\OpenIDConnectClientException
-	 */
-	public function getWellKnownConfig() {
-		if (!$this->wellKnownConfig) {
-			$well_known_config_url = \rtrim($this->getProviderURL(), '/') . '/.well-known/openid-configuration';
-			$this->wellKnownConfig = \json_decode($this->fetchURL($well_known_config_url), false);
-		}
-		return $this->wellKnownConfig;
-	}
+    /**
+     * @throws \Jumbojett\OpenIDConnectClientException
+     */
+    public function getWellKnownConfig() {
+        if (!$this->wellKnownConfig) {
+            $well_known_config_url = \rtrim($this->getProviderURL(), '/') . '/.well-known/openid-configuration';
+            $this->wellKnownConfig = \json_decode($this->fetchURL($well_known_config_url), false);
+        }
+        return $this->wellKnownConfig;
+    }
 
-	public function getUserInfo() {
-		$openIdConfig = $this->getOpenIdConfig();
-		if (isset($openIdConfig['use-access-token-payload-for-user-info']) && $openIdConfig['use-access-token-payload-for-user-info']) {
-			return $this->getAccessTokenPayload();
-		}
+    public function getUserInfo() {
+        $openIdConfig = $this->getOpenIdConfig();
+        if (isset($openIdConfig['use-access-token-payload-for-user-info']) && $openIdConfig['use-access-token-payload-for-user-info']) {
+            return $this->getAccessTokenPayload();
+        }
 
-		return $this->requestUserInfo();
-	}
+        return $this->requestUserInfo();
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function startSession() {
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function startSession() {
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function setSessionKey($key, $value) {
-		$this->session->set($key, $value);
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function setSessionKey($key, $value) {
+        $this->session->set($key, $value);
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function getSessionKey($key) {
-		return $this->session->get($key);
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function getSessionKey($key) {
+        return $this->session->get($key);
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function unsetSessionKey($key) {
-		$this->session->remove($key);
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function unsetSessionKey($key) {
+        $this->session->remove($key);
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function commitSession() {
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function commitSession() {
+    }
 
-	/**
-	 * @codeCoverageIgnore
-	 */
-	protected function fetchURL($url, $post_body = null, $headers = []) {
-		// TODO: see how to use ownCloud HttpClient ....
-		return parent::fetchURL($url, $post_body, $headers);
-	}
-
-	/**
-	 * @codeCoverageIgnore
-	 */
-	public function authenticate() : bool {
-		$redirectUrl = $this->generator->linkToRouteAbsolute('openidconnect.loginFlow.login');
-
-		$openIdConfig = $this->getOpenIdConfig();
-		if (isset($openIdConfig['redirect-url'])) {
-			$redirectUrl = $openIdConfig['redirect-url'];
-		}
-
-		$this->setRedirectURL($redirectUrl);
-		return parent::authenticate();
-	}
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function fetchURL($url, $post_body = null, $headers = []) {
+        // TODO: see how to use ownCloud HttpClient ....
+        return parent::fetchURL($url, $post_body, $headers);
+    }
 }
--- lib/Controller/LoginFlowController.php
+++ lib/Controller/LoginFlowController.php
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 namespace OCA\OpenIdConnect\Controller;
 
 use Jumbojett\OpenIDConnectClientException;
@@ -33,203 +34,217 @@ use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\RedirectResponse;
 use OCP\AppFramework\Http\Response;
 use OCP\ICacheFactory;
+use OCP\IConfig;
 use OCP\ILogger;
 use OCP\IRequest;
 use OCP\ISession;
+use OCP\IURLGenerator;
 use OCP\IUserSession;
 use OCP\Util;
 
-class LoginFlowController extends Controller {
+class LoginFlowController extends Controller
+{
 
-	/**
-	 * @var ISession
-	 */
-	private $session;
-	/**
-	 * @var UserLookupService
-	 */
-	private $userLookup;
-	/**
-	 * @var Session
-	 */
-	private $userSession;
-	/**
-	 * @var Client
-	 */
-	private $client;
-	/**
-	 * @var ILogger
-	 */
-	private $logger;
-	/**
-	 * @var ICacheFactory
-	 */
-	private $memCacheFactory;
+    /**
+     * @var ISession
+     */
+    private $session;
+    /**
+     * @var UserLookupService
+     */
+    private $userLookup;
+    /**
+     * @var Session
+     */
+    private $userSession;
+    /**
+     * @var Client
+     */
+    private $client;
+    /**
+     * @var ILogger
+     */
+    private $logger;
+    /**
+     * @var ICacheFactory
+     */
+    private $memCacheFactory;
 
-	public function __construct(string $appName,
-								IRequest $request,
-								UserLookupService $userLookup,
-								IUserSession $userSession,
-								ISession $session,
-								ILogger $logger,
-								Client $client,
-								ICacheFactory $memCacheFactory
-	) {
-		parent::__construct($appName, $request);
-		if (!$userSession instanceof Session) {
-			throw new \Exception('We rely on internal implementation!');
-		}
+    public function __construct(string $appName,
+                                IRequest $request,
+                                UserLookupService $userLookup,
+                                IUserSession $userSession,
+                                ISession $session,
+                                ILogger $logger,
+                                Client $client,
+                                ICacheFactory $memCacheFactory,
+                                IConfig $config,
+                                IURLGenerator $generator
+    )
+    {
+        parent::__construct($appName, $request);
+        if (!$userSession instanceof Session) {
+            throw new \Exception('We rely on internal implementation!');
+        }
 
-		$this->session = $session;
-		$this->userLookup = $userLookup;
-		$this->userSession = $userSession;
-		$this->client = $client;
-		$this->logger = new Logger($logger);
-		$this->memCacheFactory = $memCacheFactory;
-	}
+        $this->session = $session;
+        $this->userLookup = $userLookup;
+        $this->userSession = $userSession;
+        $this->client = $client;
+        $this->logger = new Logger($logger);
+        $this->memCacheFactory = $memCacheFactory;
 
-	/**
-	 * @NoCSRFRequired
-	 * @NoAdminRequired
-	 * @PublicPage
-	 * @CORS
-	 */
-	public function config(): JSONResponse {
-		$openid = $this->getOpenIdConnectClient();
-		if (!$openid) {
-			return new JSONResponse([]);
-		}
-		$wellKnownData = $openid->getWellKnownConfig();
-		return new JSONResponse($wellKnownData);
-	}
+        $openIdConfig = $config->getSystemValue('openid-connect', null);
+        $this->client->setRedirectURL(isset($openIdConfig['redirect-url']) ? $openIdConfig['redirect-url'] : $generator->linkToRouteAbsolute('openidconnect.loginFlow.login'));
+    }
 
-	/**
-	 * @NoCSRFRequired
-	 * @NoAdminRequired
-	 * @PublicPage
-	 * @UseSession
-	 *
-	 * @throws HintException
-	 * @throws \Jumbojett\OpenIDConnectClientException
-	 * @throws LoginException
-	 */
-	public function login(): RedirectResponse {
-		$this->logger->debug('Entering LoginFlowController::login');
-		$openid = $this->getOpenIdConnectClient();
-		if (!$openid) {
-			throw new HintException('Configuration issue in openidconnect app');
-		}
-		try {
-			$this->logger->debug('Before openid->authenticate');
-			$openid->authenticate();
-		} catch (OpenIDConnectClientException $ex) {
-			$this->logger->logException($ex);
-			throw new HintException('Error in OpenIdConnect:' . $ex->getMessage());
-		}
-		$debugInfo = \json_encode([
-			'access_token' => $openid->getAccessToken(),
-			'refresh_token' => $openid->getRefreshToken(),
-			'id_token' => $openid->getIdToken(),
-			'access_token_payload' => $openid->getAccessTokenPayload(),
-		], JSON_PRETTY_PRINT);
-		$this->logger->debug('LoginFlowController::login : Token info: ' . $debugInfo);
+    /**
+     * @NoCSRFRequired
+     * @NoAdminRequired
+     * @PublicPage
+     * @CORS
+     */
+    public function config(): JSONResponse
+    {
+        $openid = $this->getOpenIdConnectClient();
+        if (!$openid) {
+            return new JSONResponse([]);
+        }
+        $wellKnownData = $openid->getWellKnownConfig();
+        return new JSONResponse($wellKnownData);
+    }
 
-		$userInfo = $openid->getUserInfo();
-		$this->logger->debug('User info: ' . \json_encode($userInfo));
-		if (!$userInfo) {
-			throw new LoginException('No user information available.');
-		}
-		$user = $this->userLookup->lookupUser($userInfo);
+    /**
+     * @NoCSRFRequired
+     * @NoAdminRequired
+     * @PublicPage
+     * @UseSession
+     *
+     * @throws HintException
+     * @throws \Jumbojett\OpenIDConnectClientException
+     * @throws LoginException
+     */
+    public function login(): RedirectResponse
+    {
+        $this->logger->debug('Entering LoginFlowController::login');
+        $openid = $this->getOpenIdConnectClient();
+        if (!$openid) {
+            throw new HintException('Configuration issue in openidconnect app');
+        }
+        try {
+            $this->logger->debug('Before openid->authenticate');
+            $openid->authenticate();
+        } catch (OpenIDConnectClientException $ex) {
+            $this->logger->logException($ex);
+            throw new HintException('Error in OpenIdConnect:' . $ex->getMessage());
+        }
+        $debugInfo = \json_encode([
+            'access_token' => $openid->getAccessToken(),
+            'refresh_token' => $openid->getRefreshToken(),
+            'id_token' => $openid->getIdToken(),
+            'access_token_payload' => $openid->getAccessTokenPayload(),
+        ], JSON_PRETTY_PRINT);
+        $this->logger->debug('LoginFlowController::login : Token info: ' . $debugInfo);
 
-		// trigger login process
-		if ($this->userSession->createSessionToken($this->request, $user->getUID(), $user->getUID()) &&
-			$this->userSession->loginUser($user, null)) {
-			$this->session->set('oca.openid-connect.id-token', $openid->getIdToken());
-			$this->session->set('oca.openid-connect.access-token', $openid->getAccessToken());
-			$this->session->set('oca.openid-connect.refresh-token', $openid->getRefreshToken());
+        $userInfo = $openid->getUserInfo();
+        $this->logger->debug('User info: ' . \json_encode($userInfo));
+        if (!$userInfo) {
+            throw new LoginException('No user information available.');
+        }
+        $user = $this->userLookup->lookupUser($userInfo);
 
-			/* @phan-suppress-next-line PhanTypeExpectedObjectPropAccess */
-			if (isset($openid->getIdTokenPayload()->sid)) {
-				/* @phan-suppress-next-line PhanTypeExpectedObjectPropAccess */
-				$sid = $openid->getIdTokenPayload()->sid;
-				$this->session->set('oca.openid-connect.session-id', $sid);
-				$this->memCacheFactory
-					->create('oca.openid-connect.sessions')
-					->set($sid, true);
-			} else {
-				$this->logger->debug('Id token holds no sid: ' . \json_encode($openid->getIdTokenPayload()));
-			}
-			return new RedirectResponse($this->getDefaultUrl());
-		}
-		$this->logger->error("Unable to login {$user->getUID()}");
-		return new RedirectResponse('/');
-	}
+        // trigger login process
+        if ($this->userSession->createSessionToken($this->request, $user->getUID(), $user->getUID()) &&
+            $this->userSession->loginUser($user, null)) {
+            $this->session->set('oca.openid-connect.id-token', $openid->getIdToken());
+            $this->session->set('oca.openid-connect.access-token', $openid->getAccessToken());
+            $this->session->set('oca.openid-connect.refresh-token', $openid->getRefreshToken());
 
-	/**
-	 * @NoCSRFRequired
-	 * @NoAdminRequired
-	 * @PublicPage
-	 * @UseSession
-	 * @param string|null $iss
-	 * @param string|null $sid
-	 * @return Response
-	 */
-	public function logout($iss = null, $sid = null): Response {
-		// fail fast if not configured
-		$openIdConfig = $this->client->getOpenIdConfig();
-		if ($openIdConfig === null) {
-			$this->logger->warning('OpenID::logout: OpenID is not properly configured');
-			return new Response();
-		}
-		// there is an active session -> logout
-		if ($this->userSession->isLoggedIn()) {
-			$user = $this->userSession->getUser() ? $this->userSession->getUser()->getUID() : '-unknown-user-';
-			$this->logger->debug("OpenID::logout: There is an active session -> performing logout for $user");
-			// complete logout
-			$this->userSession->logout();
-		} else {
-			if ($iss === null || $sid === null) {
-				$this->logger->warning("OpenID::logout: missing parameters: iss={$iss} and sid={$sid} and no active session");
-			}
-		}
-		if ($iss === null || $sid === null) {
-			return new Response();
-		}
-		if (isset($openIdConfig['provider-url'])) {
-			if (!Util::isSameDomain($openIdConfig['provider-url'], $iss)) {
-				$this->logger->warning("OpenID::logout: iss {$iss} !== provider-url {$openIdConfig['provider-url']}");
-				return new Response();
-			}
-		}
+            /* @phan-suppress-next-line PhanTypeExpectedObjectPropAccess */
+            if (isset($openid->getIdTokenPayload()->sid)) {
+                /* @phan-suppress-next-line PhanTypeExpectedObjectPropAccess */
+                $sid = $openid->getIdTokenPayload()->sid;
+                $this->session->set('oca.openid-connect.session-id', $sid);
+                $this->memCacheFactory
+                    ->create('oca.openid-connect.sessions')
+                    ->set($sid, true);
+            } else {
+                $this->logger->debug('Id token holds no sid: ' . \json_encode($openid->getIdTokenPayload()));
+            }
+            return new RedirectResponse($this->getDefaultUrl());
+        }
+        $this->logger->error("Unable to login {$user->getUID()}");
+        return new RedirectResponse('/');
+    }
 
-		$this->memCacheFactory
-			->create('oca.openid-connect.sessions')
-			->remove($sid);
+    /**
+     * @NoCSRFRequired
+     * @NoAdminRequired
+     * @PublicPage
+     * @UseSession
+     * @param string|null $iss
+     * @param string|null $sid
+     * @return Response
+     */
+    public function logout($iss = null, $sid = null): Response
+    {
+        // fail fast if not configured
+        $openIdConfig = $this->client->getOpenIdConfig();
+        if ($openIdConfig === null) {
+            $this->logger->warning('OpenID::logout: OpenID is not properly configured');
+            return new Response();
+        }
+        // there is an active session -> logout
+        if ($this->userSession->isLoggedIn()) {
+            $user = $this->userSession->getUser() ? $this->userSession->getUser()->getUID() : '-unknown-user-';
+            $this->logger->debug("OpenID::logout: There is an active session -> performing logout for $user");
+            // complete logout
+            $this->userSession->logout();
+        } else {
+            if ($iss === null || $sid === null) {
+                $this->logger->warning("OpenID::logout: missing parameters: iss={$iss} and sid={$sid} and no active session");
+            }
+        }
+        if ($iss === null || $sid === null) {
+            return new Response();
+        }
+        if (isset($openIdConfig['provider-url'])) {
+            if (!Util::isSameDomain($openIdConfig['provider-url'], $iss)) {
+                $this->logger->warning("OpenID::logout: iss {$iss} !== provider-url {$openIdConfig['provider-url']}");
+                return new Response();
+            }
+        }
 
-		$this->logger->warning("OpenID::logout: session terminated: iss={$iss} and sid={$sid}");
+        $this->memCacheFactory
+            ->create('oca.openid-connect.sessions')
+            ->remove($sid);
 
-		$resp = new Response();
-		$resp->setHeaders([
-			'Cache-Control' =>  'no-cache, no-store',
-			'Pragma' => 'no-cache'
-		]);
-		return $resp;
-	}
+        $this->logger->warning("OpenID::logout: session terminated: iss={$iss} and sid={$sid}");
 
-	/**
-	 * @return string
-	 */
-	protected function getDefaultUrl(): string {
-		return \OC_Util::getDefaultPageUrl();
-	}
+        $resp = new Response();
+        $resp->setHeaders([
+            'Cache-Control' => 'no-cache, no-store',
+            'Pragma' => 'no-cache'
+        ]);
+        return $resp;
+    }
 
-	/**
-	 * @return Client|null
-	 */
-	private function getOpenIdConnectClient() {
-		if ($this->client->getOpenIdConfig() === null) {
-			return null;
-		}
-		return $this->client;
-	}
+    /**
+     * @return string
+     */
+    protected function getDefaultUrl(): string
+    {
+        return \OC_Util::getDefaultPageUrl();
+    }
+
+    /**
+     * @return Client|null
+     */
+    private function getOpenIdConnectClient()
+    {
+        if ($this->client->getOpenIdConfig() === null) {
+            return null;
+        }
+        return $this->client;
+    }
 }
